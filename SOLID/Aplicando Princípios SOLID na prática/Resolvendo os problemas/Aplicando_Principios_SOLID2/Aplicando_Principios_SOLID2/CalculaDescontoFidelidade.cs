6076def311ac52ad6c831159de6815e78dc3ef8e
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2026-01-14 16:55:14 -0300

    criação(): Criando a interface que define a assinatura do método de aplicar desconto feidelidade do cliente, criando a clase em que realmente é feito o cálculo, que implementa a interface criada e defini a lógica de cálculo. criada também a classe enum dos Stauts das contas dos clientes.
    AjustandoOCodigoAoPrincipioSRP

---
+++ SOLID/Aplicando Princípios SOLID na prática/Resolvendo os problemas/Aplicando_Principios_SOLID2/Aplicando_Principios_SOLID2/CalculaDescontoFidelidade.cs
@@ -0,0 +1,13 @@
+﻿namespace Aplicando_Principios_SOLID2
+{
+    class CalculaDescontoFidelidade : ICalculaDescontoFidelidade
+    {
+        public decimal AplicarDescontoFidelidade(decimal preco, int tempoDeContaEmAnos)
+        {
+            decimal descontoPorFidelidade = (tempoDeContaEmAnos > Constantes.DESCONTO_MAXIMO_POR_FEDELIDADE) ?
+                (decimal)Constantes.DESCONTO_MAXIMO_POR_FEDELIDADE / 100 : (decimal)tempoDeContaEmAnos / 100;
+            
+            return preco - (preco * descontoPorFidelidade);
+        }
+    }
+}

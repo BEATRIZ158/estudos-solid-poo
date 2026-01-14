e1d71cdf13865cbecb0ea9278874e422e58d2755
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2026-01-14 16:08:28 -0300

    modificação(GerenciadorDeDescontos): Modificando os cálculos de preço depois do desconto, melhorando a legibilidade do código e evitando duplicação do cálculo.
    ResolvendoProblemasComCleanCode

--- SOLID/Aplicando Princípios SOLID na prática/Resolvendo os problemas/Aplicando_Principios_SOLID2/Aplicando_Principios_SOLID2/GerenciadorDeDescontos.cs
+++ SOLID/Aplicando Princípios SOLID na prática/Resolvendo os problemas/Aplicando_Principios_SOLID2/Aplicando_Principios_SOLID2/GerenciadorDeDescontos.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Aplicando_Principios_SOLID2
+﻿namespace Aplicando_Principios_SOLID2
 {
     public enum StatusContaCliente
     {
@@ -25,16 +23,19 @@ namespace Aplicando_Principios_SOLID2
                     precoDepoisDoDesconto = preco;
                     break;
                 case StatusContaCliente.ClienteComum:
-                    precoDepoisDoDesconto = (preco - (0.1m * preco)) -
-                        descontoPorFidelidade * (preco - (0.1m * preco));
+                    precoDepoisDoDesconto = (preco - (0.1m * preco));
+                    precoDepoisDoDesconto = precoDepoisDoDesconto -
+                        (descontoPorFidelidade * precoDepoisDoDesconto);
                     break;
                 case StatusContaCliente.ClienteEspecial:
-                    precoDepoisDoDesconto = (0.7m * preco) -
-                        descontoPorFidelidade * (0.7m * preco);
+                    precoDepoisDoDesconto = (0.3m * preco);
+                    precoDepoisDoDesconto = precoDepoisDoDesconto -
+                        (descontoPorFidelidade * precoDepoisDoDesconto);
                     break;
                 case StatusContaCliente.ClienteVIP:
-                    precoDepoisDoDesconto = (preco - (0.5m * preco)) -
-                        descontoPorFidelidade * (preco - (0.5m * preco));
+                    precoDepoisDoDesconto = (preco - (0.5m * preco));
+                    precoDepoisDoDesconto = precoDepoisDoDesconto -
+                        (descontoPorFidelidade * precoDepoisDoDesconto);
                     break;
                 default:
                     throw new NotImplementedException();

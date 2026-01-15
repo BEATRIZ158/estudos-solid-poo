befd8043e896146e70a4ed2799db896b3e417281
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2026-01-15 15:34:40 -0300

    criação(Exercicios): Criando o exercício 2.
    Exercicios

---
+++ SOLID/Aplicando Princípios SOLID na prática/Exercicios/Projeto_Exercicio2/CursoFoop_Solid_Exercicio2/CursoFoop_Solid_Exercicio2/ConsoleLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoFoop_Solid_Exercicio2
+{
+    class ConsoleLogger 
+    {
+        public void Registrar(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}

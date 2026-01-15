0529929d1dacd9b529070c446e2ea1068a28c9a8
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2026-01-14 22:56:22 -0300

    criação(Aplicando_Principios_SOLID2): Criando a classe fábrica que cria os objetos de acordo com status da conta.
    AjustandoOCodigoAoPrincipioOCP

---
+++ SOLID/Aplicando Princípios SOLID na prática/Resolvendo os problemas/Aplicando_Principios_SOLID2/Aplicando_Principios_SOLID2/CalculaDescontoStatusContaFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Aplicando_Principios_SOLID2
+{
+    public class CalculaDescontoStatusContaFactory : ICalculaDescontoStatusContaFactory
+    {
+        public ICalculaDescontoStatusConta GetCalculaDescontoStatusConta(StatusContaCliente statusContaCliente)
+        {
+            ICalculaDescontoStatusConta calcular;
+            switch(statusContaCliente)
+            {
+                case StatusContaCliente.NaoRegistrado:
+                    calcular = new ClienteNaoRegistrado();
+                    break;
+                case StatusContaCliente.ClienteComum:
+                    calcular = new ClienteComum();
+                    break;
+                case StatusContaCliente.ClienteEspecial:
+                    calcular = new ClienteEspecial();
+                    break;
+                case StatusContaCliente.ClienteVIP:
+                    calcular = new ClienteVIP();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            return calcular;
+        }
+    }
+}

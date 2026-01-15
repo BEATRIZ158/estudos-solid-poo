59d57e80d9b2e5f86229cb10585b2c9f4efb99cd
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2026-01-15 17:02:29 -0300

    criação(Exercicios): criando o exercício 5 e aplicando os princípios SOLID.
    Exercicios

---
+++ SOLID/Aplicando Princípios SOLID na prática/Exercicios/Projeto_Exercicio5/cursoFoop_Exer_SOLID5/cursoFoop_Exer_SOLID5/PizzaMussarela.cs
@@ -0,0 +1,15 @@
+﻿namespace cursoFoop_Exer_SOLID5
+{
+    internal class PizzaMussarela : Pizza
+    {
+        public PizzaMussarela(string nome) : base(nome)
+        { }
+
+        public override int AssarPizza()
+        {
+            var tempo = 20;
+            Console.WriteLine($"Assando a pizza de {Nome} por {tempo} minutos.");
+            return tempo;
+        }
+    }
+}
